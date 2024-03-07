77a128b72176fbfb3d0b6e25c11e48c21d87f11e
JDiegx <JDiegx@users.noreply.github.com>
2024-03-07 17:26:09 -0500

    Update Repository

--- Java/CesdeCRM/src/main/java/Conexion/TestConexion.java
+++ Java/CesdeCRM/src/main/java/Conexion/TestConexion.java
@@ -1,4 +1,4 @@
-package CRUD;
+package Conexion;
 
 import java.sql.Connection;
 
@@ -6,8 +6,8 @@ public class TestConexion {
 
     public static void main(String[] args) {
 
-        miLibreria.Conexion conexion = new miLibreria.Conexion();
-        //creamos un tray con recursos osea que recibe parametros
+        Conexion conexion = new Conexion();
+
         try (Connection connect = conexion.get_connection()){
 
         }catch (Exception e){
